fa870ee4a537c7d824690d6e6f922431dbd8cebd
darthmaim <git@darthmaim.de>
2024-02-22 21:33:49 +0100

    Handle closing tags with format description

---
+++ .changeset/many-sloths-love.md
@@ -0,0 +1,5 @@
+---
+"gw2-tooltip-html": patch
+---
+
+Handle closing tags with format description
--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export function format(text: string | undefined | null) {
     return 'string' !== typeof text ? '' : text
         .replace(/<c=#([^>]+)>([^]*?)(<\/?c>|$)/g, 
             `${OPEN_TAG}span class="color-format" style="color:#$1"${CLOSE_TAG}$2${OPEN_TAG}/span${CLOSE_TAG}`)
-        .replace(/<c[=@][@=]?([^>]+)>([^]*?)(<\/?c\/?>|$)/g,
+        .replace(/<c[=@][@=]?([^>]+)>([^]*?)(<\/?c\/?(=@?[^>]+)?>|$)/g,
             `${OPEN_TAG}span class="color-format--$1"${CLOSE_TAG}$2${OPEN_TAG}/span${CLOSE_TAG}`)
         .replace(/<br\/?>/g, `${OPEN_TAG}br/${CLOSE_TAG}`)
         .replace(/\n/g, `${OPEN_TAG}br/${CLOSE_TAG}`)
